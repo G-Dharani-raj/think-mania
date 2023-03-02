0fbf010f79d01103ed27857e515667c87f014237
BhaveshKumar <bk769221@gmail.com>
2023-03-02 21:33:43 +0530

    loader

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,18 @@
+export const Loader = () => {
+  return (
+    <div
+      style={{
+        background: "linear-gradient(to right, #33ccff 0%, #ff99cc 100%)",
+        width: "100%",
+        height: "100vh",
+        margin: "0px",
+        padding: "0px",
+      }}
+    >
+      {/* <img
+        src="https://blog.logrocket.com/wp-content/uploads/2021/01/Best-React-animation-library-Top-7-libraries-compared.png"
+        alt=""
+      /> */}
+    </div>
+  );
+};
