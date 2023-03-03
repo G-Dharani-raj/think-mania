2f44a7a2990dcc9948a2316f14b9014f01d60581
G-Dharani-raj <gdharaniraj0@gmail.com>
2023-03-03 21:24:44 +0530

    env changes

--- backend/index.js
+++ backend/index.js
@@ -121,7 +121,7 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(8080, () => {
+server.listen(process.env.PORT || 8080, () => {
 	console.log("listening on 8080");
 	connectDB();
 });
