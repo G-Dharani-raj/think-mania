7314523e68b29236e53d548bc84d4d49d34f03e0
BhaveshKumar <bk769221@gmail.com>
2023-03-04 15:31:10 +0530

    win lose functionality added

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import background from "../assets/2921.jpg";
 import win from "../assets/win_board.png";
 import lose from "../assets/lose_board_1.png";
+import { SkipNavLink, SkipNavContent } from "@chakra-ui/skip-nav";
 import {
   Modal,
   ModalOverlay,
@@ -25,8 +26,8 @@ const Result: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
   const navigate = useNavigate();
   const home = () => {
-    localStorage.removeItem('username')
-    localStorage.removeItem('scores')
+    localStorage.removeItem("username");
+    localStorage.removeItem("scores");
     navigate("/menu");
   };
   const winScores = Number(localStorage.getItem("scores"));
@@ -37,34 +38,48 @@ const Result: React.FC = () => {
       backgroundImage={`url(${background})`}
       backgroundSize="cover"
       backgroundRepeat="no-repeat"
-         h={"100vh"}
+      h={"100vh"}
       filter=" brightness(70%)"
     >
       <Button onClick={home}>Home</Button>
-{winScores >= 4?
-      <Box  margin={"auto"} backgroundImage={`url(${win})`}
-           w={'30%'} display="flex"  justifyContent="center" alignItems={'center'} 
-           flexDirection={"column"} gap="10px"
+
+      {winScores >= 4 ? (
+        <Box
+          margin={"auto"}
+          backgroundImage={`url(${win})`}
+          w={"30%"}
+          display="flex"
+          justifyContent="center"
+          alignItems={"center"}
+          flexDirection={"column"}
+          gap="10px"
           backgroundSize={{ base: "cover", md: "cover", lg: "cover" }}
           backgroundPosition="center"
           backgroundRepeat="no-repeat"
           h={{ base: "80vh", md: "80vh", lg: "90vh" }}
-          >
-       <h1>{ winUser} </h1>
-       <h1>{ winScores} </h1>
-      </Box>
-      :
-      <Box  margin={"auto"} backgroundImage={`url(${lose})`}
-           w={'30%'} display="flex"  justifyContent="center" alignItems={'center'} 
-           flexDirection={"column"} gap="10px"
+        >
+          <h1>{winUser} </h1>
+          <h1>{winScores} </h1>
+        </Box>
+      ) : (
+        <Box
+          margin={"auto"}
+          backgroundImage={`url(${lose})`}
+          w={"30%"}
+          display="flex"
+          justifyContent="center"
+          alignItems={"center"}
+          flexDirection={"column"}
+          gap="10px"
           backgroundSize={{ base: "cover", md: "cover", lg: "cover" }}
           backgroundPosition="center"
           backgroundRepeat="no-repeat"
           h={{ base: "80vh", md: "80vh", lg: "90vh" }}
-          >
-       <h1>{ winUser} </h1>
-       <h1>{ winScores} </h1>
-      </Box>}
+        >
+          <h1>{winUser} </h1>
+          <h1>{winScores} </h1>
+        </Box>
+      )}
     </Box>
   );
 };
